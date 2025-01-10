3deb820951d048af60f747b235cf6421be2f9c66
Sirajuddin <106702924+sirajuddin5@users.noreply.github.com>
2025-01-11 01:12:53 +0530

    Update movie_model.dart

--- lib/model/movie_model.dart
+++ lib/model/movie_model.dart
@@ -1,22 +1,4 @@
-// class Movie {
-//   final String title;
-//   final String? imageUrl;
-//   final String description;
-//
-//   Movie({
-//     required this.title,
-//     this.imageUrl,
-//     required this.description,
-//   });
-//
-//   factory Movie.fromJson(Map<String, dynamic> json) {
-//     return Movie(
-//       title: json['name'] ?? 'No title',
-//       imageUrl: json['image'] != null ? json['image']['medium'] : null,
-//       description: json['summary'] ?? 'No description available',
-//     );
-//   }
-// }
+
 
 
 class Movie {
